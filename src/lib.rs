b45a086b6a52e0d0d14849b5c80727d6ca3218bd
Ovie Okeh <kevinokeh@gmail.com>
2023-08-11 11:24:48 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
-use web_sys::WebGlProgram;
-use web_sys::{WebGlRenderingContext, WebGlShader};
+use web_sys::{WebGlRenderingContext, WebGlShader, WebGlProgram};
 extern crate js_sys;
 
 pub fn init_webgl_context(canvas_id: &str) -> Result<WebGlRenderingContext, JsValue> {
